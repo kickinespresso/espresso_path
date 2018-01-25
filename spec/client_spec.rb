e4cc05cc6943fced127266b6466fa0046d17f89e
Chris Desch <cdesch@gmail.com>
2018-01-25 15:34:41 -0500

    Removing GeoPath class and Adding mock specs
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 require 'espresso_path'
 require 'espresso_path/client'
 include EspressoPath
-RSpec.describe EspressoPath::Client do
 
+RSpec.describe EspressoPath::Client do
 
   let(:client) { Client.new }
 
@@ -11,5 +11,16 @@ RSpec.describe EspressoPath::Client do
     client.day_parts
   end
 
+  it "gets panels" do
+    client.panels
+  end
+
+  it "gets markets" do
+    client.panels
+  end
+
+  it "gets operators" do
+    client.panels
+  end
 
 end
